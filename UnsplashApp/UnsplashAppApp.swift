68e8133f728ddaa3c70855698f84647edd10e424
Arthur <arthur.le.devedec@gmail.com>
2024-01-23 13:46:26 +0100

    Initial Commit

---
+++ UnsplashApp/UnsplashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAppApp.swift
+//  UnsplashApp
+//
+//  Created by Arthur LE-DEVEDEC on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
